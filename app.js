eeb50f585dbd75dacccdff2d2e2a244cfa4de839
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-09-30 15:37:23 -0700

    made subtract calculator calculate subtractly

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // import functions and grab DOM elements
 import { add } from './calculations.js';
+import { subtract } from './calculations.js';
 
 const addInput1 = document.getElementById('add-input-1');
 const addInput2 = document.getElementById('add-input-2');
@@ -15,6 +16,21 @@ addButton.addEventListener('click', ()=> {
     
     addOutput.textContent = result;
 });
+
+const subtractInput1 = document.getElementById('subtract-input-1');
+const subtractInput2 = document.getElementById('subtract-input-2');
+const subtractButton = document.getElementById('subtract-button');
+const subtractAnswer = document.getElementById('subtract-answer');
+
+subtractButton.addEventListener('click', ()=> {
+    const value1 = Number(subtractInput1.value);
+    const value2 = Number(subtractInput2.value);
+    const answer = subtract(value1, value2);
+
+    subtractAnswer.textContent = answer;
+});
+
+
 
 // set event listeners 
   // get user input
