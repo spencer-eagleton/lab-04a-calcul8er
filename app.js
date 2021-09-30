ab7c9824d973af849096d1675ca9abbff4001e31
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-09-30 14:32:35 -0700

    got add function working and got result to appear in span

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 // import functions and grab DOM elements
+import { add } from './calculations.js';
+
+const addInput1 = document.getElementById('add-input-1');
+const addInput2 = document.getElementById('add-input-2');
+const addButton = document.getElementById('add-button');
+const addOutput = document.getElementById('add-output');
+
 
 // initialize global state
-
+addButton.addEventListener('click', ()=> {
+    const value1 = Number(addInput1.value);
+    const value2 = Number(addInput2.value);
+    const result = add(value1, value2);
+    
+    addOutput.textContent = result;
+});
 // set event listeners 
   // get user input
   // use user input to update state 
